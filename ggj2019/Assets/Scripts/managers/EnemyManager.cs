1ad954219742cb37a835f0bfc0092e8ca82b0658
xLisu <pocztalis@icloud.com>
2019-01-26 19:23:40 +0100

    Changes

--- ggj2019/Assets/Scripts/managers/EnemyManager.cs
+++ ggj2019/Assets/Scripts/managers/EnemyManager.cs
@@ -4,30 +4,68 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
     public GameObject enemy_1;
-    //public GameObject enemy_2;
-
+    public GameObject enemy_2;
+    public int wave;
     public Health playerHP;
 
     public float spawnTime = 3f;
-    public Transform[] spawnPoints;
-    
+    public Transform[] spawnPointsM;
+    public Transform[] spawnPointsR;
+
+    float currentScore;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
-    }
 
+    }
+    private void Update()
+    {
+        if (playerHP.currentHP <= 0)
+            return;
+        if (currentScore >= 200)
+        {
+            wave = 3;
+        }else if(currentScore >= 100)
+        {
+            wave = 2;
+        }else if(currentScore >= 20)
+        {
+            wave = 1;
+        }
+        Spawn();
+    }
     // For the First Enemy
     void Spawn()
     {
         if (playerHP.currentHP <= 0)
-        {
             return;
+        switch (wave)
+        {
+            case 2:
+                wave2();
+                break;
+            case 1:
+                wave1();
+                break;
         }
+        void wave1()
+        {
+            Debug.Log("wywolana");
+        }
+        void wave2()
+        {
 
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        Instantiate(enemy_1, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        };
 
+    }
+    public void EnableEnemy()
+    {
+
+    }
+    public void DisableEnemy()
+    {
+        EnemyMovenemt enemyMovenemt = GetComponent<EnemyMovenemt>();
+        enemyMovenemt.enabled = false;
     }
 
    
--- ggj2019/Assets/Scripts/managers/GameManager.cs
+++ ggj2019/Assets/Scripts/managers/GameManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [HideInInspector] public static int score = 0;
+    [HideInInspector] public static int highScore = 0;
+
+    Health playerHP;
+    EnemyManager enemyManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerHP.currentHP <= 0)
+        {
+            RoundEnding();
+        }
+    }
+    void RoundEnding()
+    {
+        enemyManager.DisableEnemy(); 
     }
 }
